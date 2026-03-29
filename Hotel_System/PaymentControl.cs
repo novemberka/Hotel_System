3af402e11e5ba676c459e1ff68e883d8caad9e21
Thai Lineth <thailinet08@gmail.com>
2026-03-29 21:26:17 +0700

    pull from vicheka

--- Hotel_System/PaymentControl.cs
+++ Hotel_System/PaymentControl.cs
@@ -73,13 +73,10 @@ namespace Hotel_System
         {
 
         }
-<<<<<<< HEAD
 
         private void btnPrint_Click(object sender, EventArgs e)
         {
 
         }
-=======
->>>>>>> 445b99bcfe4e84349f3c85d532352d6ca27b264b
     }
 }
